244d042e5dbc6e8ecc9f1d24ad4ae734edf83cd2
tongzai-git <597145585@qq.com>
2025-05-09 11:00:51 +0800

    Simplify build configuration

--- next.config.mjs
+++ next.config.mjs
@@ -5,25 +5,6 @@ const nextConfig = {
   basePath: '/beauty',
   images: {
     unoptimized: true,
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**',
-      },
-    ],
-  },
-  transpilePackages: ['three', '@react-three/fiber', '@react-three/drei'],
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.(glb|gltf)$/,
-      use: {
-        loader: 'file-loader',
-      },
-    });
-    return config;
-  },
-  experimental: {
-    esmExternals: 'loose',
   },
   typescript: {
     ignoreBuildErrors: true,
